3918fbbbcef8058ed55dd3c0c30bd259830a4d19
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-08 00:24:34 +0100

    fist

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 # setup a web server
-
 sudo apt-get update
 sudo apt-get install nginx -y
-
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-sudo echo "its working" > /data/web_static/releases/test/index.html
-
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home - AirBnB clone</title>
+    </head>
+    <body>
+        <h1>Welcome to AirBnB clone - Deploy static!</h1>
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
-
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-
 sudo service nginx restart
