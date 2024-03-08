432a8477c30f1e19d76a84d44bcf0753b010b56a
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-08 01:05:15 +0100

    2nd

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#/usr/bin/python
+""" this scprit generate .tgz from content of wen static """
+
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    """ generate a .tgz """
+    
+    if not os.path.exists('versions'):
+        local("mkdir -p versions")
+    
+    time = datetime.now().strftime('%y%m%d%h%m%s')
+    name = f"web_static_{time}.tgz"
+
+    output = local(f"tar -cf versions/{name} web_static", capture=True)
+    if output.failed:
+        return None
+    else:
+        return os.path.join("versions", name)
