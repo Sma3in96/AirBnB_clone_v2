7e652da52ac82b155931897209f3a4d5927fe096
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-08 02:20:34 +0100

    3rd

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' distribute the archive to the web servers '''
+from fabric.api import put, run, env
+import os
+
+env.hosts = ['18.234.193.16', '100.25.183.7']
+
+
+def do_deploy(archive_path):
+    """ deploy the web static """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        file = archive_path.split("/")[-1]
+        ext_d = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run(f"mkdir -p /data/web_static/releases/{ext_d}/")
+        run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{ext_d}/")
+        run(f"rm /tmp/{file}")
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, ext_d))
+        run(f'rm -rf {path}{ext_d}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f"ln -s {path}{ext_d}/ /data/web_static/current")
+        return True
+    except Exception as e:
+        return False
