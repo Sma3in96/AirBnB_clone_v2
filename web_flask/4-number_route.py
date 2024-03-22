b46147166db282009d24d9ef0d1a0949cde57630
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-22 05:24:34 +0000

    task4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -40,11 +40,10 @@ def pth():
     return "Python is cool"
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """ print n if its an integer"""
-    if isinstance(n, int):
-        return f'{n} is a number'
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
