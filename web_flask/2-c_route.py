40b297f89ca04f3cff1cfe6122619ca83b2a476a
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-22 04:10:36 +0000

    task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" start a web app """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def Hello():
+    """ print hello at route root """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    """ print hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET', 'POST'], strict_slashes=False)
+def textfunc(text):
+    """ print a passed text"""
+    text = text.replace('_', ' ')
+    return f"C is {text}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
